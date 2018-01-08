7649c077840412e5e356ed1a1c9f91425eb02f63
Daniel Cabale <dcabale.dc@gmail.com>
2018-01-08 14:37:45 +0800

    convert timestamp

--- public/app.js
+++ public/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Select from 'react-select';
+import moment from 'moment';
 
 class App extends React.Component {
 	constructor(props) {
@@ -25,17 +26,18 @@ class App extends React.Component {
 	}
 	getData(val) {
 		if(val){
-			this.state.value = val;
 		}
+		var oldData = this.state.items;
 		fetch(`http://localhost:3000/api/products?_sort=${this.state.value}&_page=${this.state.page}&_limit=30`)
 		.then(res => res.json())
 		.then((res) => {
-			console.log(res);
+			var newData = this.state.items.concat(res);
 			this.setState({
 				isLoaded: true,
-				items: res,
+				items: newData,
 				value: this.state.value,
-				loading: false
+				loading: false,
+				requestSent: false
 			})
 		}),
 		(error) => {
@@ -62,16 +64,15 @@ class App extends React.Component {
 		// use jQuery
 		$.ajax({
 			url: "#",
-			data: null,
+			data: '',
 			method: "GET",
 			success: function(data, textStatus, jqXHR) {
-				var data = this.getData(this.state.items.length);
-				var newData = this.state.items.concat(data);
-				this.setState({data: newData, requestSent: false});
+				var data = this.getData();
 			}.bind(this),
 			error: function(jqXHR, textStatus, errorThrown) {
 				this.setState({requestSent: false});
 			}.bind(this)
+
 		});
 	}
 	handleScroll() {
@@ -85,38 +86,77 @@ class App extends React.Component {
 		}
 	}
 	handleChange() {
+		this.setState({requestSent: true});
 		var	val = this.refs.dropValue.value
 		this.setState({
-			value: this.refs.dropValue.value
+			value: this.refs.dropValue.value,
+			items: []
 		});
 		this.getData(val);
 	}
+	convertDate(dates) {
+		var newDate = moment(dates).fromNow();
+		var date1 = newDate.split(" ");
+		console.log(newDate);
+		if(date1[1] == "days" && date1[0] > 7) {
+			newDate = moment(dates).format("DD-MM-YYYY");
+		}
+		return newDate;
+	}
 	render() {
 		const { error, isLoaded, items } = this.state;
+		var keys = 0;
+		var aDay = 24*60*60*1000
+		console.log({ items });
 		return (
 			<div className="col-md-12">
-			<div className="col-md-12">
-			SORT BY: <select ref="dropValue" onChange={(e) => {this.handleChange();} } value={this.state.value}>
-			<option value="id">ID</option>
-			<option value="size">Size</option>
-			<option value="price">Price</option>
-			</select>
-			</div>
-			<div className="col-md-12">
-			{items.map(item => (
-				<div className="col-md-3 product-items" key={item.id}>
-				<div className="prod-face">
-				{item.face}
+				<div className="col-md-12">
+					SORT BY: <select ref="dropValue" onChange={(e) => {this.handleChange();} } value={this.state.value}>
+						<option value="id">ID</option>
+						<option value="size">Size</option>
+							<option value="price">Price</option>
+					</select>
 				</div>
-				<div className="prod-price">
-				Price: ${item.price}
+				<div className="col-md-12">
+					{items.map(item => (
+					<div className="col-md-3 product-items" key={item.id}>
+						<div className="prod-face">
+							{item.face}
+						</div>
+						<div className="prod-price">
+							Price: ${item.price}
+						</div>
+						<div className="prod-size">
+							Size: {item.size}kb
+						</div>
+						<div className="prod-size">
+							Date: {this.convertDate(item.date)}
+						</div>
+					</div>
+					))}
 				</div>
-				<div className="prod-size">
-				Size: {item.size}kb
+        {(() => {
+          if (this.state.requestSent) {
+            return(
+              <div className="data-loading">
+				<div id="floatingBarsG">
+					<div class="blockG" id="rotateG_01"></div>
+					<div class="blockG" id="rotateG_02"></div>
+					<div class="blockG" id="rotateG_03"></div>
+					<div class="blockG" id="rotateG_04"></div>
+					<div class="blockG" id="rotateG_05"></div>
+					<div class="blockG" id="rotateG_06"></div>
+					<div class="blockG" id="rotateG_07"></div>
+					<div class="blockG" id="rotateG_08"></div>
 				</div>
-				</div>
-			))}
-			</div>
+              </div>
+            );
+          } else {
+            return(
+              <div className="data-loading"></div>
+            );
+          }
+        })()}
 			</div>
 		);
 	}
